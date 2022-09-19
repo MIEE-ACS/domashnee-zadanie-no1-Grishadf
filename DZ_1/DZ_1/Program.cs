31acf12acfdc0452d0825830eb9f4ccc9217d196
Grishadf <74361972+Grishadf@users.noreply.github.com>
2022-09-19 21:49:06 +0300

    Add files via upload

---
+++ DZ_1/DZ_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 12. Найти значение функции y =  4*(x-3)^6 - 7*(x-3)^3 + 2
+
+using System;
+
+namespace DZ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для функции:          y =  4*(x-3)^6 - 7*(x-3)^3 + 2 ");
+            Console.Write("Введите значение:     x =  ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Значение функции:     y =  { 4 * Math.Pow(x - 3, 6) - 7 * Math.Pow(x - 3, 2) + 2 }");
+        }
+    }
+}
+
